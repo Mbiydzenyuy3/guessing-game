888063c11e2a2beb52743eab64d13c2892dc6f7a
Debugger <mbiydzenyuyeileen@gmail.com>
2024-11-28 15:16:20 +0100

    commiting javascript logic added to the guessing game

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 // Generate a random number between 1 and 100
-const secretNumber = 33;
+const secretNumber = 33
 
 // Initialize variables
 let attempts = 0
@@ -14,7 +14,7 @@ const attemptsElement = document.getElementById('attemps')
 submitButton.addEventListener('click', checkGuess)
 
 // Function to check the user's guess
-function checkGuess() {
+function checkGuess () {
   // Get the user's guess and convert it to a number
   const userGuess = parseInt(guessInput.value)
   attempts++
@@ -22,14 +22,14 @@ function checkGuess() {
   // Check if the guess is correct
   if (userGuess === secretNumber) {
     hintElement.textContent = `Congratulations! You guessed the correct number in ${attempts} attempts.`
-    disableGame()
+    disableGame ()
   } else if (attempts === maxAttempts) {
     hintElement.textContent = `Game over! The correct number was ${33}.`
-    disableGame()
+    disableGame ()
   } else if (userGuess < secretNumber) {
-    hintElement.textContent = "Too low! Try a higher number.";
+    hintElement.textContent = 'Too low! Try a higher number.'
   } else {
-    hintElement.textContent = "Too high! Try a lower number.";
+    hintElement.textContent = 'Too high! Try a lower number.'
   }
 
   // Update attempts display
@@ -40,7 +40,7 @@ function checkGuess() {
 }
 
 // Function to disable the game after it ends
-function disableGame() {
+function disableGame () {
   guessInput.disabled = true
   submitButton.disabled = true
 }
